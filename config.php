917c3d4bc45ec1573a0fed4e7bd3c96215862914
gscfwid <gscfwid@gmail.com>
2017-07-18 22:59:48 +0800

    UI_updated_2
    UI_updated_2

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 	header("Content-type: text/html; charset=utf-8");
 	define('HOST', '127.0.0.1');
 	define('PORT','3306');
-	define('USERNAME', 'root');
-	define('PASSWORD', '');
+	define('USERNAME', 'name_of_db');
+	define('PASSWORD', 'password_of_db');
 ?>
